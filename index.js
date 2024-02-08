112d981687214b36f5d0a01dfb9e266533b310ac
Tarun <57524319+tarun19991121@users.noreply.github.com>
2024-02-08 19:03:42 +0530

    Create index.html

---
+++ index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <title>Fylo Landing Page With Two Column Layout Master</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+  <h1>Fylo Landing Page With Two Column Layout Master</h1>
+  </body>
+</html>
